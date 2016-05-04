4b7720fc5c7aef3bda0db4c8750bb36e9bbe4ba6
Haonan <wuhaonan@eluying.com>
2016-05-04 18:50:42 +0800

    add constants and OrderStatus
    add constants and OrderStatus

---
+++ src/main/java/com/atlsmall/common/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.atlsmall.common.constants;
+
+/**
+ * Created by haonan on 16/5/2.
+ */
+public class Constants {
+
+}
---
+++ src/main/java/com/atlsmall/common/enums/OrderStatus.java
@@ -0,0 +1,18 @@
+package com.atlsmall.common.enums;
+
+/**
+ * Created by haonan on 16/5/2.
+ */
+public enum OrderStatus {
+    PAID("A", "已付款"),
+    EXPRESSING("I", "已发货"),
+    CANCELED("C", "订单取消"),
+    FINIDSHED("D", "已收货"),
+    ;
+    String code;
+    String msg;
+    OrderStatus(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+}
